d3ae06f464a061296ef591b3695112f1c4e9fa9c
Anirban Roy Das <Roy@Anirbans-MacBook.local>
2016-04-30 21:33:12 +0530

    added app.py and date.js

---
+++ app.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+    Simple sockjs-tornado chat application. By default will listen on port 9090.
+"""
+import tornado.ioloop
+import tornado.web
+import tornado.escape
+
+from sockjs.tornado import SockJSConnection, SockJSRouter
+from multiplex import MultiplexConnection
+
+
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    """Regular HTTP handler to serve the chatroom page"""
+    def get(self):
+        self.render('index.html')
+
+
+# multiplex.js static handler
+class MultiplexStaticHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('multiplex.js')
+
+
+
+# Connections
+participants=set()
+
+class groupConnection(SockJSConnection):
+    def on_open(self, info):
+        
+	    
+		participants.add(self)
+
+    def on_message(self, message):
+    	res=tornado.escape.json_decode(message)
+    	if res['isFirst']==1:
+    		self.broadcast(participants, '*** '+res[name]+' joined ***')
+    	else:
+        	self.broadcast(participants, '['+res[name]+']  ' + res[message])
+ 
+
+    def on_close(self):
+    	participants.remove(self)
+    	self.broadcast(participants,'*** somebody left ***')
+	    
+	    
+
+	
+
+class privateConnection(SockJSConnection):
+    def on_open(self, info):
+        self.send('Carl says goodbye!')
+		participants.add(self)
+
+    def on_message(self,message):
+	    pass
+
+	
+    def on_close(self):
+	    participants.remove(self)
+	    self.broadcast(participants,'Carl Left')
+
+
+
+if __name__ == "__main__":
+    import logging
+    logging.getLogger().setLevel(logging.DEBUG)
+
+    # Create multiplexer
+    router = MultiplexConnection.get(group=groupConnection, private=privateConnection)
+    print "router : ",router
+    # Register multiplexer
+    EchoRouter = SockJSRouter(router, '/chat')
+    print "echorouter.ursl : ",EchoRouter.urls
+    # Create application
+    app = tornado.web.Application(
+            [(r"/", IndexHandler), (r"/multiplex.js", MultiplexStaticHandler)] + EchoRouter.urls
+    )
+    app.listen(9999)
+
+    tornado.ioloop.IOLoop.instance().start()
